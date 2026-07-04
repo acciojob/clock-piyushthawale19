6ad77408cce2e434ba3003c243dd4c4eebf5cebc
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-07-04 15:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const td = document.getElementById("timer");
+
+setInterval(function () {
+    const currentDate = new Date();
+    td.innerHTML = currentDate.toLocaleString();
+}, 1000);
